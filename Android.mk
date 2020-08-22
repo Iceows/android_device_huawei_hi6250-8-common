6591dba7a07068ca6a817f530837c75d07cf8795
derfelot <5124143+derfelot@users.noreply.github.com>
2020-08-22 11:23:31 +0200

    hi6250: create vulkan/opencl symlinks to libGLES_mali.so
    Change-Id: Ie26232f9599e1a3f97ccc3e3ee6d8600ce03ba64

--- Android.mk
+++ Android.mk
@@ -16,6 +16,59 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter prague warsaw, $(TARGET_DEVICE)),)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+ifneq ($(filter prague warsaw,$(TARGET_DEVICE)),)
+include $(CLEAR_VARS)
+
+# OpenCL Symlinks
+LIBOPENCL1_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libOpenCL.so.1
+$(LIBOPENCL1_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib/libOpenCL.so.1 symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf $(TARGET_OUT_VENDOR)/lib/libOpenCL.so $@
+
+LIBOPENCL641_SYMLINK := $(TARGET_OUT_VENDOR)/lib64/libOpenCL.so.1
+$(LIBOPENCL641_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib64/libOpenCL.so.1 symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf $(TARGET_OUT_VENDOR)/lib64/libOpenCL.so $@
+
+LIBOPENCL11_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libOpenCL.so.1.1
+$(LIBOPENCL11_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib/libOpenCL.so.1.1 symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf $(TARGET_OUT_VENDOR)/lib/libOpenCL.so.1 $@
+
+LIBOPENCL6411_SYMLINK := $(TARGET_OUT_VENDOR)/lib64/libOpenCL.so.1.1
+$(LIBOPENCL6411_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib64/libOpenCL.so.1.1 symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf $(TARGET_OUT_VENDOR)/lib64/libOpenCL.so.1 $@
+
+ALL_DEFAULT_INSTALLED_MODULES += \
+	$(LIBOPENCL1_SYMLINK) \
+	$(LIBOPENCL641_SYMLINK) \
+	$(LIBOPENCL11_SYMLINK) \
+	$(LIBOPENCL6411_SYMLINK)
+
+# Vulkan Symlinks
+LIBGLES_MALI_LIBRARY := /vendor/lib/egl/libGLES_mali.so
+LIBGLES_MALI64_LIBRARY := /vendor/lib64/egl/libGLES_mali.so
+
+VULKAN_SYMLINK := $(TARGET_OUT_VENDOR)/lib/hw/vulkan.hi6250.so
+$(VULKAN_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib/hw/vulkan.hi6250.so symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf $(LIBGLES_MALI_LIBRARY) $@
+
+VULKAN64_SYMLINK := $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.hi6250.so
+$(VULKAN64_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating lib64/hw/vulkan.hi6250.so symlink: $@"
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf $(LIBGLES_MALI64_LIBRARY) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += \
+	$(VULKAN_SYMLINK) \
+	$(VULKAN64_SYMLINK)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
