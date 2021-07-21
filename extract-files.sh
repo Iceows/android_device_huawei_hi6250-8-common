9b2b13df9329f7fdd2a981c63ae04f6d657316dc
A2L5E0X1 <alexed25@protonmail.com>
2021-07-21 15:00:05 +0200

    hi6250: link libutilscallstack to libGLES_mali and gralloc
    * Could not load /vendor/lib64/egl/libGLES_mali.so from sphal namespace: dlopen failed: cannot locate symbol "_ZN7android9CallStackC1EPKci" referenced by "/vendor/lib64/egl/libGLES_mali.so"....
    
    Change-Id: I4f1969dd35b96ac7bf40d4fe71ddfcb2fe6a3435

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+	vendor/lib64/egl/libGLES_mali.so|vendor/lib64/hw/gralloc.hi6250.so)
+	    "${PATCHELF}" --add-needed "libutilscallstack.so" "${2}"
+	    ;;
+    esac
+}
+
 if [ -z "${ONLY_TARGET}" ]; then
     # Initialize the helper for common device
     setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
