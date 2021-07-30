8d24f59113581b0318093345e6f629e617fc8dec
A2L5E0X1 <alexed25@protonmail.com>
2021-07-31 01:07:44 +0200

    hi6250: build bcm bluetooth hal
    Change-Id: If7d1f89e265722d9b0dd2032e2684516c5b70e9f

--- hi6250.mk
+++ hi6250.mk
@@ -26,7 +26,8 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.0-service \
+    libbt-vendor
 
 # Camera
 PRODUCT_PACKAGES += \
