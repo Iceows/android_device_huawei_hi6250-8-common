6bbd8e02302a3565b39414b56c20992b52d3ab08
Kostyan_nsk <kostyan_nsk@ngs.ru>
2021-11-14 11:29:58 +0100

    hi6250: import opensource lights hal
    Change-Id: Ieffe37a6bf480518f9fa8b594980611d43118efe

--- hi6250.mk
+++ hi6250.mk
@@ -103,7 +103,8 @@ PRODUCT_PACKAGES += \
 # Light
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.light@2.0-service \
+    lights.hi6250
 
 # MAC Loader
 PRODUCT_PACKAGES += \
