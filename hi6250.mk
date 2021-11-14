hi6250-8/android_device_huawei_hi6250-common

init: import init.usb.configfs.rc from stock

hi6250.mk
@@ -116,6 +116,7 @@ PRODUCT_PACKAGES += \
     init.device.rc \
     init.extmodem.rc \
     init.hi6250.rc \
+    init.hi6250.usb.rc \
     init.hisi.rc \
     init.ko.rc \
     init.manufacture.rc \

Iceows/android_device_huawei_hi6250-8-common

Change-Id: I6f6e7ec08e98a6c002edc1535b9407bc3f26ed0e

hi6250.mk
@@ -80,6 +80,7 @@ PRODUCT_PACKAGES += \
     init.extmodem.rc \
     init.hi6250.rc \
     init.hisi.rc \
+    init.hisi.usb.rc \
     init.ko.rc \
     init.manufacture.rc \
     init.performance.rc \