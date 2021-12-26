aa127bf3c6e6474471b6c0296a51dcd1b0bb1b01
A2L5E0X1 <alexed25@protonmail.com>
2021-12-26 20:23:14 +0100

    hi6250: block twrp from installing this rom
    * We don't want to support TWRP and it's data fuckups.
    
    Change-Id: I918ca6f349e7c9d2f652b37086970800651abe60

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,4 +1,5 @@
 #!/bin/env python3
+
 #
 # Copyright (C) 2021-2022 The LineageOS Project
 #
@@ -7,6 +8,14 @@
 
 import common
 import re
+
+def FullOTA_Assertions(info):
+  OTA_Assertions(info)
+  return
+
+def IncrementalOTA_Assertions(info):
+  OTA_Assertions(info)
+  return
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
@@ -21,6 +30,10 @@ def AddImage(info, dir, basename, dest):
   common.ZipWriteStr(info.output_zip, basename, data)
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+
+def OTA_Assertions(info):
+  info.script.AppendExtra('assert(getprop("ro.twrp.boot") == "" || abort("ERROR: TWRP detected! Please use provided Lineage Recovery! For more info, read t.me/hi6250_oss/12."););')
+  return
 
 def OTA_InstallEnd(info):
   AddImage(info, "RADIO", "boot_ramdisk.img", "/dev/block/by-name/ramdisk")
