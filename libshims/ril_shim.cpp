d644924cbd0f4bf7560dab6ec38e35fc6bff174d
R0rt1z2 <rogerortizleal@gmail.com>
2022-01-23 21:03:59 +0100

    hi6250: libshims: shim _ZN7android8hardware5radio4V1_08toStringENS2_9CallStateE
    Change-Id: I047e9571f8bc676dd432bda525c34e5d198ec7fe

--- libshims/ril_shim.cpp
+++ libshims/ril_shim.cpp
@@ -4,4 +4,8 @@ extern "C" {
     const char *_ZN7android8hardware5radio4V1_08toStringENS2_13PersoSubstateE(int SubaddressType) {
         std::string buf(""); buf.append(std::to_string(SubaddressType)); return buf.c_str();
     }
+
+    const char *_ZN7android8hardware5radio4V1_08toStringENS2_9CallStateE(int CallState) {
+        std::string buf(""); buf.append(std::to_string(CallState)); return buf.c_str();
+    }
 }
