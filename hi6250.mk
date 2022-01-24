22991842cf59a0dbbc06a8855085742b9ec43e65
A2L5E0X1 <alexed25@protonmail.com>
2022-01-24 18:07:33 +0100

    hi6250: proprietary: switch to stock bt hal
    * bcm bt hal never worked on hi6250
    
    Change-Id: Ic77fdc40f62bee0e203fc458211ddcf83ccb379f

--- hi6250.mk
+++ hi6250.mk
@@ -39,8 +39,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service \
-    libbt-vendor
+    android.hardware.bluetooth@1.0-service
 
 # Camera
 PRODUCT_PACKAGES += \
