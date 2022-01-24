d83c1e19cc8c834568ff89f7368eefaa9a04c1bc
A2L5E0X1 <alexed25@protonmail.com>
2022-01-24 23:00:25 +0100

    hi6250: cleanup: drop depreacted/dead boardconfig flags and build targets
    Change-Id: I7e0bd49836acdf4eecf27ea0548564d1b9d661c1

--- hi6250.mk
+++ hi6250.mk
@@ -67,7 +67,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.renderscript@1.0-impl \
     libion
 
 # Health
