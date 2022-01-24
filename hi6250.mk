b20392c14fab4b41cacacea97784c49ca06c4963
Arian <arian.kulmer@web.de>
2022-01-24 22:04:22 +0100

    hi6250: build AOSP NFC stack
    Change-Id: I076b3efb7a085822778f01b592d55982e67d7f6e

--- hi6250.mk
+++ hi6250.mk
@@ -144,13 +144,10 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
-    android.hardware.nfc@1.0-service \
+    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
-    libnqnfc-nci \
-    libp61-jcop-kit \
-    nfc_nci.nqx.default \
     NfcNci \
+    SecureElement \
     Tag
 
 # Permissions
