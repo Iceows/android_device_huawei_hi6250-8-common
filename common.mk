a471a38513682cc755e6cd5a8bda2be779d7dcfa
Raphael Mounier <mounierr07@gmail.com>
2024-01-13 23:06:36 +0100

    audio: use hisi wrapper

--- common.mk
+++ common.mk
@@ -38,24 +38,23 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.service.hisi \
-    android.hardware.audio@4.0-impl-hisi \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
-    audio.r_submix.default \
-    audio.usb.default \
-    libaudio-resampler \
-    libaudiopreprocessing \
-    libtinycompress \
-    libaudio-resampler \
-    libtinycompress \
-    libalsautils    
-
+    android.hardware.audio.service.hisi \
+    android.hardware.bluetooth.audio@2.1-impl
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.1-impl \
-    audio.bluetooth.default
-   
+    audio.primary.hisi_wrapper \
+    audio.bluetooth.default \
+    audio.r_submix.default \
+    audio.usb.default
+
+PRODUCT_PACKAGES += \
+    libaudiopreprocessing \
+    libtinycompress \
+    libalsautils
+
+
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
